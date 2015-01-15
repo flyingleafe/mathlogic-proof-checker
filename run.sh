f158af193ffb932c0de7f162133e42c79d9a2156
flyingleafe <flyingleafe@gmail.com>
2015-01-15 22:51:12 +0300

    script enhancements and readme changes

--- run.sh
+++ run.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-cabal run $1 -- inputs/$1/$2 outputs/$1/$3
+function run {
+    echo "processing file: $2.in -> $2.out... "
+    time ./dist/build/$1/$1 inputs/$1/$2.in outputs/$1/$2.out
+}
+
+if [ $# -eq 2 ]
+then
+    run $1 $2
+elif [ $# -eq 1 ]
+then
+    inputs=$(find inputs/$1/*.in -exec basename {} .in \;)
+    for i in $inputs
+    do
+        run $1 $i
+    done
+else 
+    echo "Wrong number of arguments"
+fi
